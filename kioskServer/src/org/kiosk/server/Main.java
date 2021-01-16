b5d925e021603005d0b6e9778afc48b0c3886e65
zerock code1 <zkang075@gmail.com>
2021-01-17 01:54:26 +0900

    OOP kiosk Server

---
+++ kioskServer/src/org/kiosk/server/Main.java
@@ -0,0 +1,24 @@
+package org.kiosk.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kiosk.server.task.MenuTask;
+import org.kiosk.server.task.OrderTask;
+import org.kiosk.server.task.Task;
+
+public class Main {
+
+	public static void main(String[] args)throws Exception {
+		
+		Map<String, Task> taskMap = new HashMap<>();
+		
+		taskMap.put("M", new MenuTask());
+		taskMap.put("O", new OrderTask());
+		
+		StoreServer storeServer = new StoreServer(taskMap);
+		
+		storeServer.runServer();
+		
+	}
+}
